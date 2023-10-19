a1bab3ed1bea9c0c3f88af271590793a980414d8
Kristina <kristi_7_82@inbox.ru>
2023-10-19 16:05:47 +0300

    chekking4

--- Multithreading/ParsingMultithreading/src/main/java/LinkFinder.java
+++ Multithreading/ParsingMultithreading/src/main/java/LinkFinder.java
@@ -8,43 +8,41 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.RecursiveTask;
 
 public class LinkFinder extends RecursiveTask<Set<String>> {
+//    public class LinkFinder extends RecursiveTask<String> {
 
-private static CopyOnWriteArrayList<String> linksFromTags = new CopyOnWriteArrayList<>();
-
+    private static CopyOnWriteArrayList<String> linksFromTags = new CopyOnWriteArrayList<>();
     public String url;
     public String link = "";//Почему бы не хранить ссылку просто в переменной типа строка
 
+    Set<String> links = new HashSet<>();
+
     public LinkFinder(String url) {//в методе compute мы парсим все ссылки со страницы, которая была передана в конструкторе
-        this.url = url.trim();//инициализирую ссылку (присваиваю значение в переменную string)
+        this.url = url;//инициализирую ссылку (присваиваю значение в переменную string)
     }
 
     @Override
+//    protected String compute() {
     protected Set<String> compute() {
         Set<LinkFinder> tasks = new HashSet<>();//это список запущенных
-        Set<String> links = new HashSet<>();
         try {
-            Thread.sleep(150);
-            Document document = Jsoup.connect(url).ignoreHttpErrors(true).get();//выставила игнорирование ошибок
-            Elements elements = document.select("a");
-
-//            if ((!url.isEmpty() && !linksFromTags.contains(url) && !url.contains("#"))) {
-//                linksFromTags.add(link);
-//            }
+            Thread.sleep(200);
+            Document document = Jsoup.connect(url).ignoreContentType(true).ignoreHttpErrors(true).get();//выставила игнорирование ошибок
+            Elements elements = document.select("a[href*=/]");
 
             elements.forEach(el -> {
                 String item = el.attr("abs:href");
                 if ((!url.isEmpty() && !linksFromTags.contains(url) && !url.contains("#"))) {
                     linksFromTags.add(item);
                     for (String s : linksFromTags) {
-                        LinkFinder task = new LinkFinder(item);//создаём новый экземпляр RecursiveAction, передавая в конструктор эту ссылку
-                        task.fork();//делаем на ней fork (то есть запускаю эту задачу асинхронно)
-                        tasks.add(task);//и добавляем эту задачу в список запущенных
-                        //linksFromTags.add(item);//это и будет нам заменой класса Node вместо этой строки node.addChild(new Node(link));
+                        LinkFinder finder = new LinkFinder(s);//создаём новый экземпляр RecursiveAction, передавая в конструктор эту ссылку
+                        finder.fork();//делаем на ней fork (то есть запускаю эту задачу асинхронно)
+                        tasks.add(finder);//и добавляем эту задачу в список запущенных
                     }
                 }
             });
 
             for (LinkFinder task : tasks) {//После перебора всех ссылок проходим по списку запущенных задач
+//                link = task.join();
                 links.addAll(task.join());
                 //links = task.join();//и соберем результат работы с помощью join()
             }
@@ -53,10 +51,26 @@ private static CopyOnWriteArrayList<String> linksFromTags = new CopyOnWriteArray
             e.printStackTrace();
         }
         return links;
+//        return link;
     }
+
+
+
+
 
     public String getUrl() {
         return url;
     }
 
+    public static CopyOnWriteArrayList<String> getLinksFromTags() {
+        return linksFromTags;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder(getUrl());
+        getLinksFromTags().forEach(child -> result.append("\n").append(child));
+        return result.toString();
+    }
 }
--- Multithreading/ParsingMultithreading/src/main/java/Main.java
+++ Multithreading/ParsingMultithreading/src/main/java/Main.java
@@ -1,21 +1,37 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     //public static String URL = "https://www.lenta.ru";
     public static String URL = "https://www.dnk.ru";
+    //public static String URL = "https://www.skillbox.ru";
+    //public static String URL = "https://www.meteoclub.ru";
 
     public static void main(String[] args) {
 
-        System.out.println("Старт" + URL);
+        System.out.println("Старт" + " " + URL);
         LinkFinder link = new LinkFinder(URL);
         System.out.println("Запущен пул");
         ForkJoinPool pool1 = new ForkJoinPool();
         pool1.invoke(link);
         System.out.println("Finish");
 
-        System.out.println(" ");
+
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("data/file.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        writer.write(link.toString());
+        writer.flush();
+        writer.close();
     }
 }
+
+
+
 
 
 
